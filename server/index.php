e0d035c3ef3a05dbc7dd767717873fb6d29002a5
anhthu100903 <anhthu115599@gmail.com>
2024-03-10 23:55:04 +0700

    getDsProject

--- server/index.php
+++ server/index.php
@@ -1,27 +1,27 @@
 <?php
+  use Psr\Http\Message\ResponseInterface as Response;
+  use Psr\Http\Message\ServerRequestInterface as Request;
+  use Slim\Factory\AppFactory;
+  require_once __DIR__ . '/vendor/autoload.php';
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-require_once __DIR__ . '/vendor/autoload.php';
+  require_once "./storage/PDOManager.php";
+  require_once "./storage/ProjectStorage.php";
+  require_once "./service/ProjectService.php";
+  require_once "./controller/ProjectController.php";
+echo "hiih";
+  $app = AppFactory::create();
 
-require_once "./storage/PDOManager.php";
-require_once "./storage/ProjectStorage.php";
-require_once "./service/ProjectService.php";
-require_once "./controller/ProjectController.php";
+  $db = new PDOManager(null);
+  $projectStore = new ProjectStorage($db);
+  $projectService = new ProjectService($projectStore);
+  $projectController = new ProjectController($projectService);
 
-$app = AppFactory::create();
-
-$db = new PDOManager(null);
-$projectStore = new ProjectStorage($db);
-$projectService = new ProjectService($projectStore);
-$projectController = new ProjectController($projectService);
-
-$app->post("/v1/projects", function(Request $req, Response $res) use ($projectController){
+  $app->post("/v1/projects", function(Request $req, Response $res) use ($projectController){
   return $projectController->CreateProject($req, $res);
-});
+  });
 
-$app->get("/v1/projects", function(Request $req, Response $res) use ($projectController){
+  $app->get("/v1/projects", function(Request $req, Response $res) use ($projectController){
   return $projectController->getAllProject($req, $res);
-});
-$app->run();
+  });
+  $app->run();
+?>
