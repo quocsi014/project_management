815b5a4b5d5f2ad1e0a912577a8882c83f3ccd12
anhthu100903 <anhthu115599@gmail.com>
2024-03-15 23:14:03 +0700

    fix iboard

--- server/storage/IBoardStorage.php
+++ server/storage/IBoardStorage.php
@@ -10,8 +10,5 @@ interface IBoardStorage{
   public function updateBoard(Board $board):void;
   public function updatePreviuosBoard(String $boardID, ?String $preID, ?String $newpreviousBoardID):void;
   public function getBoardsOfProject(String $projectID):array;
-  public function deleteBoard(int $projectID):void;
-  public function updatePreviuosBoard(String $previousBoardID):void;
-  public function getBoardsOfProject(int $projectID):array;
   public function deleteBoard(String $boardID):void;
 }
