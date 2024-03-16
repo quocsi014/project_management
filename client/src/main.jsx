7b8a913d3ced1f5f4f280ce98ad1476a13e33cf2
Tên Mới <quocsi014@gmail.com>
2024-03-16 13:23:50 +0700

    heroku

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
+import { RouterProvider, BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from "./pages/Login.jsx"
+import Register from './pages/Register.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>}></Route>
+        <Route path="/login" exact element={<Login/>}></Route>
+        <Route path="/register" exact element={<Register/>}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
