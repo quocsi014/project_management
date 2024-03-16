6e0e9954947f671cdc588095dfa9fb1d1c79a2f6
TranThiThuyTranThiThuy <114789828+TranThiThuyTranThiThuy@users.noreply.github.com>
2024-03-16 22:26:07 +0700

    updateStatustask

--- server/storage/ITaskStorage.php
+++ server/storage/ITaskStorage.php
@@ -7,7 +7,7 @@ use Entity\Task;
 interface ItaskStorage{
   public function inserTask(Task $task):void;
   public function updateTask(Task $task):void;
-  public function updateStatus(string $taskId, ?string $boardId, ?string $newStatus): void;
+  public function updateStatus(int $taskId, ?int $boardId, ?string $newStatus = null): void;
   public function updateAssignedUSer(String $userID):void;
   public function getTasksOfProjec(String $projectID):array;
   public function deleteTask(String $taskID):void;
